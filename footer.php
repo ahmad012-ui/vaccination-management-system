d2f76953b6a2e8bd53572aa3a1798f5bffcdef5f
Wania519 <waniadilsher@gmail.com>
2025-08-07 17:42:14 +0500

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,34 +1,18 @@
-<!-- Footer Start -->
-<div class="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.2s">
+  <!-- Footer Start -->
+  <div class="container-fluid footer py-5 wow fadeIn" data-wow-delay="0.2s">
     <div class="container py-5">
         <div class="row g-5">
             <!-- Project Info -->
             <div class="col-md-6 col-lg-6 col-xl-4">
                 <div class="footer-item d-flex flex-column">
-                    <h4 class="text-white mb-4">
-                        <i class="fas fa-syringe me-3"></i>Vaccino
-                    </h4>
-                    <p class="text-white" style="text-decoration: none;">
-                        Vaccino is an online vaccination booking and management system helping parents schedule timely vaccinations for their children with ease and convenience.
-                    </p>
+                    <h4 class="text-white mb-4"><i class="fas fa-syringe me-3"></i>Vaccino</h4>
+                    <p class="text-white" style="text-decoration: none;">Vaccino is an online vaccination booking and management system helping parents schedule timely vaccinations for their children with ease and convenience.</p>
                     <div class="d-flex align-items-center">
                         <i class="fas fa-share fa-2x text-white me-2"></i>
-                        <a class="btn-square btn text-white rounded-circle mx-1" 
-                           style="background-color:rgb(0, 204, 255);" href="#">
-                            <i class="fab fa-facebook-f"></i>
-                        </a>
-                        <a class="btn-square btn text-white rounded-circle mx-1" 
-                           style="background-color:rgb(0, 204, 255);" href="#">
-                            <i class="fab fa-twitter"></i>
-                        </a>
-                        <a class="btn-square btn text-white rounded-circle mx-1" 
-                           style="background-color:rgb(0, 204, 255);" href="#">
-                            <i class="fab fa-instagram"></i>
-                        </a>
-                        <a class="btn-square btn text-white rounded-circle mx-1" 
-                           style="background-color:rgb(0, 204, 255);" href="#">
-                            <i class="fab fa-linkedin-in"></i>
-                        </a>
+                        <a class="btn-square btn text-white rounded-circle mx-1" style="background-color:rgb(0, 204, 255);" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn-square btn text-white rounded-circle mx-1" style="background-color:rgb(0, 204, 255);" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="btn-square btn text-white rounded-circle mx-1" style="background-color:rgb(0, 204, 255);" href="#"><i class="fab fa-instagram"></i></a>
+                        <a class="btn-square btn text-white rounded-circle mx-1" style="background-color:rgb(0, 204, 255);" href="#"><i class="fab fa-linkedin-in"></i></a>
                     </div>
                 </div>
             </div>
@@ -37,21 +21,11 @@
             <div class="col-md-6 col-lg-6 col-xl-4">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-white">Quick Links</h4>
-                    <a href="about.php" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-angle-right text-white me-2"></i> About Us
-                    </a>
-                    <a href="contact.php" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-angle-right me-2"></i> Contact Us
-                    </a>
-                    <a href="#" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-angle-right me-2"></i> Privacy Policy
-                    </a>
-                    <a href="#" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-angle-right me-2"></i> Terms & Conditions
-                    </a>
-                    <a href="blog.php" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-angle-right me-2"></i> News & Updates
-                    </a>
+                    <a href="index.php" class="text-white" style="text-decoration: none;"><i class="fas fa-angle-right me-2"></i>Home</a>
+                    <a href="about.php" class="text-white" style="text-decoration: none;"><i class="fas fa-angle-right text-white me-2"></i> About Us</a>
+                    <a href="contact.php" class="text-white" style="text-decoration: none;"><i class="fas fa-angle-right me-2"></i> Contact Us</a>
+                    <a href="blog.php" class="text-white" style="text-decoration: none;"><i class="fas fa-angle-right me-2"></i> News & Updates</a>
+                    <a href="testimonial.php" class="text-white" style="text-decoration: none;"><i class="fas fa-angle-right me-2"></i> Feedback</a>
                 </div>
             </div>
 
@@ -59,44 +33,31 @@
             <div class="col-md-6 col-lg-6 col-xl-4">
                 <div class="footer-item d-flex flex-column">
                     <h4 class="mb-4 text-white">Contact Info</h4>
-                    <a href="#" class="text-white" style="text-decoration: none;">
-                        <i class="fa fa-map-marker-alt me-2"></i> Karachi, Pakistan
-                    </a>
-                    <a href="mailto:info@vaccino.com" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-envelope me-2"></i> info@vaccino.com
-                    </a>
-                    <a href="mailto:support@vaccino.com" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-envelope me-2"></i> support@vaccino.com
-                    </a>
-                    <a href="tel:+923001234567" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-phone me-2"></i> +92 300 1234567
-                    </a>
-                    <a href="#" class="text-white" style="text-decoration: none;">
-                        <i class="fas fa-print me-2"></i> +92 300 1234567
-                    </a>
+                    <a href="#"class="text-white" style="text-decoration: none;"><i class="fa fa-map-marker-alt me-2"></i> Karachi, Pakistan</a>
+                    <a href="mailto:info@vaccino.com"class="text-white" style="text-decoration: none;"><i class="fas fa-envelope me-2"></i> info@vaccino.com</a>
+                    <a href="mailto:support@vaccino.com"class="text-white" style="text-decoration: none;"><i class="fas fa-envelope me-2"></i> support@vaccino.com</a>
+                    <a href="tel:+923001234567" class="text-white" style="text-decoration: none;"><i class="fas fa-phone me-2"></i> +92 300 1234567</a>
+                    <a href="#"class="text-white" style="text-decoration: none;"><i class="fas fa-print me-2"></i> +92 300 1234567</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <!-- Footer End -->
-
-<!-- JavaScript Libraries -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="lib/wow/wow.min.js"></script>
-<script src="lib/easing/easing.min.js"></script>
-<script src="lib/waypoints/waypoints.min.js"></script>
-<script src="lib/owlcarousel/owl.carousel.min.js"></script>
-
-<!-- Template Javascript -->
-<script src="js/main.js"></script>
-
-<!-- Bootstrap Bundle JS CDN -->
+  
+        <!-- JavaScript Libraries -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<!-- Book Vaccination End -->
+ <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+        <!-- Bootstrap Bundle JS CDN -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
 <!-- jQuery if needed -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
 <!-- Custom JS -->
 <script src="js/main.js"></script>
